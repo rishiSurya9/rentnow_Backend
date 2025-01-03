2077da4b6cc5ab88f3cccacd17ea5ef98d0157d3
Rushikesh <rishisurya1320@gmail.com>
2025-01-03 19:45:22 +0530

    Login text 2

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import  signup  from '../controller/auth.controller.js';
-import login from '../controller/auth.controller.js';
+import { login, signup } from '../controller/auth.controller.js';
 
 router.post('/signup',signup);
 router.post('/login',login);
