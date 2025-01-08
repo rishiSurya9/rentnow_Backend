9b2f523102c2d1220a181e85b91c909ecf326f5c
Rushikesh <rishisurya1320@gmail.com>
2025-01-08 11:43:43 +0530

    api 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { signup , login } from '../controller/auth.controller.js';
+import authRouter from '../controller/auth.controller.js';
 import cors from 'cors';
 
 import cookieParser from 'cookie-parser';
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,4 +4,4 @@ import { login, signup } from '../controller/auth.controller.js';
 
 router.post('/signup',signup);
 router.post('/login',login);
- export default router;
+export default router;
