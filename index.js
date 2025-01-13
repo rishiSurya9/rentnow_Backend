5f9a199d0821ee55aa508a0729e576340cc6d4c0
Rushikesh <rishisurya1320@gmail.com>
2025-01-13 22:37:08 +0530

    updates

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
-import userRouter from './routes/user.route.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
@@ -48,7 +47,6 @@ app.use(cookieParser());
 
 // API routes
 app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
