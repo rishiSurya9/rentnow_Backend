9670768da3fb76ba04c0947647ff4cd980c3adb5
Rushikesh <rishisurya1320@gmail.com>
2025-01-14 21:52:22 +0530

    Google Oauth

--- models/user.model.js
+++ models/user.model.js
@@ -15,7 +15,11 @@ const userSchema = new Schema({
     password:{
         type:String,
         required:true
-    }
+    },
+    avatar:{
+        type:String,
+       default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw3gyFNHmjfgWOjb05uBtqga&ust=1736957563796000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPiSu6bN9YoDFQAAAAAdAAAAABAE"
+    },
 },
 {
     timestamps:true
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { login, signup, update } from '../controller/auth.controller.js';
+import { google, login, signup, update } from '../controller/auth.controller.js';
 
 router.post('/signup',signup);
 router.post('/login',login);
 router.post('/update',update);
+router.post('/google',google);
 
 // router.get('/google');
 // router.get('/google/callback',passport.authenticate('google', { scope: ['profile', 'email'] }),authController.googleAuth);
