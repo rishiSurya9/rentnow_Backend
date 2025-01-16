8920649be206f8b5ac453c89e805893bb2ce69fc
Rohandede1825 <rohandede97@gmail.com>
2025-01-16 20:24:42 +0530

    Update user 1

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/error"
 import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
     res.json({
@@ -18,11 +19,12 @@ export const updateUser = async (req, res, next) => {
             req.body.password =  bcryptjs.hashSync(req.body.password, 10)
         }
 
-        const updateUser= await User.findIdAndUpdate(req.params.id, {
+        const updateUser= await User.findByIdAndUpdate(req.params.id, {
             $set:{
                 username: req.body.username,
                 email: req.body.email,
                 password: req.body.password,
+                avatar:req.body.avatar
 
             }
 
@@ -30,6 +32,7 @@ export const updateUser = async (req, res, next) => {
 
         const {password, ...rest}= updateUser._doc;
         res.status(200).json(rest)
+        
     } catch (error) {
         next(error)
     }
