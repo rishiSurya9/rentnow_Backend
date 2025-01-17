5695fb6f3595419f55df1a174f836be354765e21
Rohandede1825 <rohandede97@gmail.com>
2025-01-17 19:59:34 +0530

    Change delete user route method from POST to DELETE

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 
 router.get('/test',test);
 router.post('/update/:id', verifyToken, updateUser)
-router.post('/delete/:id', verifyToken, DeleteUser)
+router.delete('/delete/:id', verifyToken, DeleteUser)
 
 
 
