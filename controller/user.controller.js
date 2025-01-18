6fc4cc6592e987faf5462d295cb20aac81090c33
Rohandede1825 <rohandede97@gmail.com>
2025-01-18 08:26:25 +0530

    Clear access token cookie before sending delete user response

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -45,7 +45,8 @@ export const DeleteUser = async (req, res, next) =>     {
 
     try {
         await User.findByIdAndDelete(req.params.id)
-        res.status(200).json('User has been deleted').clearCookie('access_token')
+        res.clearCookie('access_token')
+        res.status(200).json('User has been deleted');
     } catch (error) {
         next(error)
     }
