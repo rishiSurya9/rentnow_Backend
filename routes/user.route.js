7abd150e5c3328ee67e557d588f81eeb1e89fe99
Rushikesh <rishisurya1320@gmail.com>
2025-01-24 20:10:57 +0530

    userListing 2

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser, DeleteUser, getUserListing } from "../controllers/user.controller.js";
+import { test, updateUser, DeleteUser, getUserListing } from "../controller/user.controller.js";
 const router = express.Router();
 
 import { verifyToken } from "../utils/verifyUser.js";
