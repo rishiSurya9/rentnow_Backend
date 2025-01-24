01f993f7a8a9a52d09738de04dc95cfa5e42930c
Rushikesh <rishisurya1320@gmail.com>
2025-01-24 20:26:00 +0530

    userListing 3

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -4,6 +4,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/create",  createListing);
+router.post("/create", verifyToken, createListing);
 
 export default router;
