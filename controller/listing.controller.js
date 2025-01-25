41a42f5b77b394c5e4dfde669f335e8c7c6b1d7a
Rushikesh <rishisurya1320@gmail.com>
2025-01-25 12:48:47 +0530

    update

--- controller/listing.controller.js
+++ controller/listing.controller.js
@@ -27,3 +27,34 @@ export const createListing = async (req, res,next) => {
     next(error);
   }
 };
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (listing.userRef !== req.user.id) {
+    return next(errorHandler(401, "You can only update your own listing"));
+  }
+  try{
+   const updateListing = await Listing.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new:true}
+   );
+   res.status(200).json(updateListing);
+   
+  } catch (error) {
+    next(error);
+  }
+};
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
