7ef0e0729e7b63e12b9661ed264c63431f7ec77e
Rohandede1825 <rohandede97@gmail.com>
2025-01-26 16:15:06 +0530

    Error2

--- controller/listing.controller.js
+++ controller/listing.controller.js
@@ -65,19 +65,19 @@ export const getListings = async (req, res, next) => {
     const limit = parseInt(req.query.limit)||9;
     const startIndex = parseInt(req.query.startIndex)||0;
     let offer = req.query.offer;
-    if(offer === undedified || offer === 'false'){
+    if(offer === undefined || offer === 'false'){
       offer = {$in:[false,true]};
     }
     let furnished = req.query.furnished;
-    if(furnished === undedified || furnished === 'false'){
+    if(furnished === undefined || furnished === 'false'){
       furnished = {$in:[false,true]};
     }
     let parking = req.query.parking;
-    if(parking === undedified || parking === 'false'){
+    if(parking === undefined || parking === 'false'){
       parking = {$in:[false,true]};
     }
     let type = req.query.type;
-    if(type === undedified || type === 'all'){
+    if(type === undefined || type === 'all'){
       type = {$in:['sale','rent']};
     }
 
