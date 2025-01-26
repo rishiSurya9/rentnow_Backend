8a8b6e4882e8b83581b7c447035bfd23591e4577
Rohandede1825 <rohandede97@gmail.com>
2025-01-26 13:20:44 +0530

    Add getUser endpoint to retrieve user details by ID

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser, DeleteUser} from "../controller/user.controller.js";
+import { test, updateUser, DeleteUser, getUser} from "../controller/user.controller.js";
 const router = express.Router();
 
 import { verifyToken } from "../utils/verifyUser.js";
@@ -7,6 +7,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 router.get('/test',test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, DeleteUser)
+router.get('/:id', verifyToken, getUser);
 
 
 
