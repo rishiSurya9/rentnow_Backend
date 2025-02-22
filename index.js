c77876398d6627488f2bc8d15a257fafa9414a3b
Rushikesh <rishisurya1320@gmail.com>
2025-02-22 12:47:44 +0530

    new website

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
     'http://localhost:3000',              // Local frontend
     'https://rentnow-indol.vercel.app',   // Deployed frontend
+    'https://script-assist-alpha.vercel.app/',
+    
 ];
 
 // Configure CORS
@@ -26,7 +28,7 @@ app.use(cors({
 
 // Set custom CORS headers
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin',  'https://rentnow-indol.vercel.app'); // Allow specific frontend origin
+    res.setHeader('Access-Control-Allow-Origin',  allowedOrigins); // Allow specific frontend origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow necessary methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow necessary headers
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
