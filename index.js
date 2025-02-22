871651e631e2e2586e41387da05817a33e819a06
Rohandede1825 <rohandede97@gmail.com>
2025-02-22 16:50:29 +0530

    ajshdf

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(cors({
 
 // Set custom CORS headers
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin',  'https://script-assist-alpha.vercel.app/'); // Allow specific frontend origin
+    res.setHeader('Access-Control-Allow-Origin',  'https://rentnow-indol.vercel.app'); // Allow specific frontend origin
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow necessary methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow necessary headers
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
