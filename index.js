6723a669d472b083ada93d128f9df119f68c187c
Rohandede1825 <rohandede97@gmail.com>
2025-02-22 15:17:00 +0530

    LOGIN AUTH

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 // Allowed origins for CORS
 const allowedOrigins = [
+    'http://localhost:3000',              // Local frontend
     'https://rentnow-indol.vercel.app',   // Deployed frontend
     'https://script-assist-alpha.vercel.app/',
     
